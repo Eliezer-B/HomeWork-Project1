13e71f59a98c97d932ed93577264e86947402be7
Eliezer B <eliezer.babayev@gmail.co.il>
2022-12-12 22:05:43 +0200

    update new function

--- index.js
+++ index.js
@@ -1,49 +1,63 @@
-import {  appendFile, writeFile } from "node:fs";
-import readline from "node:readline";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+import { appendFile } from "node:fs/promises";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const rl = readline.createInterface({ input, output });
 
-const data = () =>
-  rl.question("What is your First Name ? ", (firstName) => {
-    rl.question("What is your Last Name? ", (lastName) => {
-      rl.question("What is your Age? ", (age) => {
-        rl.question("What is your ID num? ", (ID) => {
-          rl.question("What is your E-mail? ", (email) => {
-            // TODO: Log the answer in a database
-            const data = { firstName, lastName, age, ID, email };
-            const me = { name: "Adam", age: 30 };
+const strinRegex = /[A-Za-z]/g;
+const emailRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-            appendFile("data.test", "test", (err) => {
-              if (err) throw err;
-              console.log('The "data to append" was appended to file!');
-            });
 
-            console.log(
-              `Your information:  
-          First Name: ${firstName} 
-          Last Name: ${lastName} 
-          Age: ${age}
-          ID: ${ID}
-          E-mail:${email}
-          
-          `
-            );
 
-            rl.close();
-          });
-        });
-      });
-    });
-  });
+const data = async () => {
+  let firstName = await rl.question("What is your First Name ? ");
+  while (!firstName.match(strinRegex)) {
+    console.log("first name must be  string");
+    firstName = await rl.question("What is your first Name? ");
+  }
 
-data();
+  let lastName = await rl.question("What is your Last Name? ");
+  while (!lastName.match(strinRegex)) {
+    console.log("last name must be  string");
+    lastName = await rl.question("What is your last Name? ");
+  }
+
+  const dob = await rl.question("What is your DOB?(DD/MM/YYYY) ");
+
+  const ID = await rl.question("What is your ID num? ");
+
+  let email = await rl.question("What is your E-mail? ");
+
+  while (!email.match(emailRegex)) {
+    console.log("email must be valid");
+    email = await rl.question("What is your E-mail? ");
+  }
+
+  const userData = { firstName, lastName, dob, ID, email };
+ 
+
+  await appendFile("./data.txt", `${JSON.stringify(userData)}\n`);
+
+  rl.close();
+};
+
+const addOrSearchUser = async () => {
+  const addOrSearch = await rl.question(
+    "please choose Add or Search a user (press add || Search)"
+  );
+  if (addOrSearch === "add") {
+    data();
+  } else {
+    console.log("you choose to search a user ");
+  }
+
+  // addOrSearchUser()
+};
+
+addOrSearchUser();
 
 rl.on("close", function () {
   console.log("\nBYE BYE !!!");
   process.exit(0);
 });
-
-const test = "test "
